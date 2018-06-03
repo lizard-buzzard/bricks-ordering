9b40a5d529532e68bbef3e19ae7c3f94211eaf5e
Григорий Дробязко <grigory.drobyazko@avicomp.ru>
2018-06-03 17:56:05 +0300

    changed Controller behavior to let it answers a BadRequestException in case of an attempt to change already dispatched data for Stage 4

--- src/main/java/bricks/controller/CustomerOrderController.java
+++ src/main/java/bricks/controller/CustomerOrderController.java
@@ -19,39 +19,47 @@ public class CustomerOrderController {
 
     @PostMapping("/CreateOrder")
     @ResponseBody
-    public CustomerOrder create(@RequestBody CustomerOrder customerOrder)    {
+    public CustomerOrder create(@RequestBody CustomerOrder customerOrder) {
         return customerOrderRepository.save(customerOrder);
     }
 
     @GetMapping("/GetOrders")
     @ResponseBody
-    public List<CustomerOrder> findAll()    {
+    public List<CustomerOrder> findAll() {
         return customerOrderRepository.findAll();
     }
 
     @PutMapping("/UpdateOrder/{id}")
     @ResponseBody
-    public CustomerOrder update(@PathVariable("id") Long id, @RequestBody CustomerOrder customerOrder)    {
-        CustomerOrder order = customerOrderRepository.getOne(id);
-        order.setBricks(customerOrder.getBricks());
-        return customerOrderRepository.save(order);
+    public ResponseEntity<CustomerOrder> update(@PathVariable("id") Long id, @RequestBody CustomerOrder customerOrder) {
+        return customerOrderRepository.findById(id)
+                .filter(order->order.getIsDispatched().equals("no"))
+                .map(order->{
+                    order.setBricks(customerOrder.getBricks());
+                    customerOrderRepository.save(order);
+                    return new ResponseEntity<>(order, HttpStatus.OK);
+                }).orElseThrow(() -> new BadRequestException());
+
+//        CustomerOrder order = customerOrderRepository.getOne(id);
+//        order.setBricks(customerOrder.getBricks());
+//        return customerOrderRepository.save(order);
     }
 
     @GetMapping("/GetOrder/{id}")
     @ResponseBody
-    public Optional<CustomerOrder> findOrderById(@PathVariable("id") Long id)    {
+    public Optional<CustomerOrder> findOrderById(@PathVariable("id") Long id) {
         return customerOrderRepository.findById(id);
     }
 
     @PutMapping("/FulfilOrder/{id}")
     @ResponseBody
-    public ResponseEntity<CustomerOrder> fulfilOrder(@PathVariable("id") Long id, @RequestBody CustomerOrder customerOrder)   {
+    public ResponseEntity<CustomerOrder> fulfilOrder(@PathVariable("id") Long id, @RequestBody CustomerOrder customerOrder) {
         return customerOrderRepository.findById(id)
-                .map(order->{
+                .map(order -> {
                     order.setIsDispatched(customerOrder.getIsDispatched());
                     customerOrderRepository.save(order);
                     return new ResponseEntity<>(order, HttpStatus.OK);
-                }).orElseThrow(()->new BadRequestException());
+                }).orElseThrow(() -> new BadRequestException());
     }
 
 //    @DeleteMapping("/orders/{id}")
