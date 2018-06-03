404c7ac964d3e9b7cbb6928d1320745eb8094d4b
Григорий Дробязко <grigory.drobyazko@avicomp.ru>
2018-06-03 13:02:38 +0300

    test on order update for Stage 2 specification

--- src/test/test/UpdateOrderTest.java
+++ src/test/test/UpdateOrderTest.java
@@ -11,10 +11,25 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import utills.Utils;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * Stage 2
+ * As a Rest Client
+ * I want to update orders for bricks
+ * So I can update customers’ orders
+ * Given
+ * A customer has ordered a number of bricks
+ * When
+ * An "Update Order" request for an existing order reference and a number of bricks is submitted
+ * Then
+ * An Order reference is returned
+ * And
+ * The Order reference is unique to the submission
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
@@ -36,7 +51,7 @@ public class UpdateOrderTest {
     }
 
     /**
-     * Method checks that POST request change number of bricks for existed order and do not change the order ID
+     * Method checks that PUT request change number of bricks for existed order and do not change the order ID
      *
      * @throws Exception
      */
@@ -52,6 +67,8 @@ public class UpdateOrderTest {
 
         long bricksNoBeforeUpdate = Utils.getBricks(getBeforeUpdateResult.getResponse().getContentAsString());
 
+        int orderIdBeforeUpdate = Utils.getOrderId(getBeforeUpdateResult.getResponse().getContentAsString());
+
         // then update the order by other number of bricks
         MvcResult putResult = mockMvc.perform(
                 put("/bricks_api/UpdateOrder/{id}", orderToBeUpdatedId)
@@ -60,7 +77,11 @@ public class UpdateOrderTest {
                         .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk()).andReturn();
 
-        // finally, retrieve the order after update
+        int orderIdAfterUpdate = Utils.getOrderId(putResult.getResponse().getContentAsString());
+
+        assertEquals(orderIdBeforeUpdate, orderIdAfterUpdate);
+
+        // finally, retrieve again the order after update
         MvcResult getAfterUpdateResult = mockMvc.perform(
                 get("/bricks_api/GetOrder/{id}", orderToBeUpdatedId))
                 .andExpect(status().isOk()).andReturn();
